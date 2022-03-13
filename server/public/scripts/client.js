4e82b36f590ba6287232c874cf221e79f83512fb
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-03-13 17:03:28 -0500

    Added additional comments to client.js and server.js, Index.html; add: class id to clear and enter btns, Style.css; add: btton styling to buttons. Data displayed to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,99 +1,149 @@
-const { takeCoverage } = require("v8");
-
+//Log'd to show client.js loaded.
 console.log('client loaded!');
+//Log'd to show js sourced.
+console.log('js loaded!');
 
-//*
-
+//Jquery call to document initiate starting output to DOM.
 $(document).ready(handleReady);
 
-
+//Created function to handle ready tasks.
 function handleReady() {
-    console.log('JQuery ready to roll!');
-
-    //after adding a button to html and two fields...
+    //Log'd to show JQuery sourced.
+    console.log('JQuery loaded!');
+    //Jquery call to target button element in html, as displayed on the DOM.
+    //Will call functions on click.
     $('#equals').on('click', handleSubmit);
-    $('#add').on('click', addFunc);
-    $('#subtract').on('click', subFunc);
-    $('#multiply').on('click', mulFunc);
-    $('#divide').on('click', divideFunc);
+    $('.operator').on('click', handleOperator);
     $('#clear').on('click', handleClear);
 
-getData();
-}
+    //Call to getData Function to run in handleReady
+    getData();
+}//end of handleReady function
 
-//function to handleSubmit btn when clicked. added listener
+//function to handleSubmit tasks when = button is clicked on the DOM by the user.
 function handleSubmit() {
-    console.log('clicked!');
     let num1 = $('#num1').val();
     let num2 = $('#num2').val();
-    console.log(num1, num2);
-    
+    console.log('User Input (num1):', num1, 'User Input (num 2):', num2);
+    console.log('Sending dynamic data (user inputs) to server');
+
+    //AJAX!!!!!! REQ AND RESPONSE. starts from client, server responds...
+    //ajax method: POST request. Requests the get route from the server and the type request. 
     $.ajax({
         url: '/calc',
         method: 'POST',
         //data will become req.body on the server
+        //created object keys to take in user input
         data: {
             num1: num1,
             math: math,
             num2: num2
         }
-    }).then(function(response) {
+    }).then(function (response) {
+        //Log'd to show post route in server OK (res.sendStatus(200)).
         console.log(response)
-        
-        //DOM is out of sync, need to GET again, call the getData function
-        // //Call render function to render to DOM.
-        // render(response); !!!!made it show undefined in console on click then would
-        // showed appended data after second click
-        //called GetData Function to render to DOM
+        //Call to getData function to initiate in handleSubmit
         getData();
 
         //empty inputs on DOM
         $('#num1').val('');
         $('#num2').val('');
     })
-}
+}//end of handleSubmit function
 
 //Created function to clear input on button click.
-function handleClear(){
+function handleClear() {
+    //Jquery call to input elements and empty on DOM
     $('input').val('');
-    // $('#num2').val('');
-}
+}//end of handle clear function
 
-
+//Created function to get data from the server.
 //AJAX!!!!!! REQ AND RESPONSE. starts from client, server responds...
-function getData(){
-    console.log('GET dataLibrary');
-    //ajax method built into Jquery. you call it and ask it what to do...
+function getData() {
+    console.log('GET data');
+    //ajax method: GET request. Requests the get route from the server and the type of request.
     $.ajax({
-        //tells ajax we going here
-        url : '/calc',
-        //tells ajax we request something
-        method: 'GET'
-        //.then tells the server what to do after the time it takes for the request to get across the net. we made an
-        //anonymous function
-    }).then(function(response) {
-        //this is asking for the res.send(); on the server in thr app.get route...
-        console.log(response)
-        //log- ** NOW... append the data in the response to the DOM
-        
-        //Call render function to render to web pass argument
+        //direction to '/calc' route from server.
+        url: '/calc',
+        //direction to request a type of route (GET route).
+        method: 'GET'   
+    })
+
+    //Used .then to initiate server action after receipt of the GET request.
+    //Will receive res.send(answers) from the server, as directed in the app.get/answers route.
+    .then(function (response) {
+        //Log'd response from server in console 
+        console.log('data[]; (on server.js):', response);
+        //NOW... append the data in the response to the DOM
+        //Call render function to initiate in getData Function. Should render data to DOM.
         render(response);
+    })
     
-    }).catch(function(error) {
-        //this is catching errors for the res.send(); on the server in thr app.get route...
+    //Used .catch method to catch errors for the res.send() on the server in the app.get route.
+    .catch(function (error) {
+        //logged to see error incase alert isn't working.
         console.log(error);
-        alert('error in GET!')
-})
+        //created alert to display to the DOM indicating error in GET /calc request.
+        alert('error in GET /calc!');
+    })
+    //ajax method: GET request. Requests the get route from the server and the type of request.
+    $.ajax({
+        //direction to '/answers' route from server.
+        url: '/answers',
+        //direction to request a type of route (GET route).
+        method: 'GET'
+    })
 
-    console.log('end of getData Function...')
-}//end of getData
+    //Used .then method to initiate server action after receipt of the GET request.
+    //Will receive res.send(answers) from the server, as directed in the app.get/answers route.
+    .then(function (response) {
+        console.log('answers[]; (on server.js):', response);
+        //client responds with product from userAnswers function.
+        userAnswer(response);
+    })
+    
+    //Used .catch method to catch errors for the res.send() on the server in the app.get route.
+    .catch(function (error) {
+        //logged to see error incase alert isn't working.
+        console.log(error);
+        //created alert to display to the DOM indicating error in GET /answers request.
+        alert('error in GET /answers!');
+    })
+    console.log('end of getData Function');
+}//end of getData Function.
 
-function render(calculations) {
-    //wash away old data
+//Created function to render to the DOM.
+//Called in getData Function.  
+//(Calculation as an parameter), Argument is rec.body from the server side post route.
+function render(calculation) {
+    //Clear old data.
     $('#calc').empty();
-    console.log('In render Function');
-    //
-    for (const calc of calculations) {
-        $('#calc').append(`<li>${calc.num1} ${calc.num2}</li>`)
-    } 
-}
+    //Log'd to show in render function.
+    console.log('Render Function Operable');
+    //Created for of loop through the response from the server (req.body from the server side POST route).
+    for (const answer of calculation) {
+        //Jquery call to prepend to calc element in html
+        $('#calc').prepend(`<li>${answer}</li>`);
+    }
+}//end of renderFunction.
+
+//declares a variable that the handleOperator can define.
+let math = 0;
+
+//Created function to handle operator click from user.
+function handleOperator() {
+    //Jquery call to target text on click and set the value to operations. 
+    math = $(this).text();
+}//end of handleOperator function.
+
+//Created function to handle userAnswer
+//(Calculation as parameter). Argument is rec.body from the server side post route.
+function userAnswer(calculation) {
+    //clears old data to the DOM.
+    $('#results').empty();
+    //Jquery call to prepend to the answer to the users calculation to the DOM. 
+    //Since I only want to prepend the last item property, I use the object length -1.
+    $('#results').prepend(calculation[calculation.length - 1]);
+}//end of userAnswer function.
+
+
--- server/server.js
+++ server/server.js
@@ -1,76 +1,104 @@
-//requiring express
+//Installed express module in node 
+
+//Importing/requiring express library
 const express = require('express');
-//make a server
+
+//Server creation:
 const app = express();
 const PORT = 5000;
-//bodyParser is like a mail sorter... **see bodyParser app below
-//IMPORT body parser
+
+//Importing/requiring body parser
 const bodyParser = require('body-parser');
 
-const dataLibrary = [];
+//Created empty arrays to store dynamic data.
+const data = [];
 const answers = [];
 
-//**add bodyParser app to add a new line of text to the dataLibrary
+//added bodyParser app to add a new line of text to the data array
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
-
-//**GET & POST Routes go here **//
 
 // start the server
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
 
+//**POST Routes**//
+
+//POST route to localhost:5000/calc
 app.post('/calc', (req, res) => {
     console.log('POST calc', req.body);
-
+    
+    //getData Function from client side will initiate a GET request for the url assignment, /calc.
+    //The product of the calculate function becomes the req.body and will be the response from 
+    //the server to the client.
     calculate(req.body);
-    //200 ok
-    //201 CREATED
 
+    //Status code. Request is received and understood and is being processed.
     res.sendStatus(200);
 })
 
+//** GET Routes **//
 
-//-------------GET ROUTES---------------//
-//localhost:5000/calc
+//GET route to localhost:5000/calc
 app.get('/calc', (req, res) => {
-    // /calc route
-    // get route to /calc
-
-    console.log('GET /calc!');
-    //server must respond!
-    res.send(dataLibrary);
+    //log'd to ensure that route is operation.
+    console.log('GET /calc route OK');
+    //Server responds with data array that holds dynamic data. 
+    res.send(data);
 })
 
-function calculate (solution) {
+//GET route to localhost:5000/answers
+app.get('/answers', (req, res) => {
+    //log'd to ensure that route is operation.
+    console.log('GET /answers route OK');
+    //Server responds with answers array that holds dynamic data.
+    res.send(answers);
+})
+
+//Created function to calculate user input (2 numbers and a mathematics operator).
+function calculate(solution) {
+    console.log('In Calculate function');
+
+    //Created variables and set their values to the properties from the ajax req.
     let input1 = Number(solution.num1);
     let math = solution.math;
     let input2 = Number(solution.num2);
-    
+
+    //Used a switch statement to execute based on each case. The value of math (the use
+    //is checked in each case, then will execute condition and push the condition as 
+    //a string to the data array and answers array.
+    //pushed user input values to data array as a string.
+    //pushed product of calculate function to answers array. 
     switch (math) {
         case '+':
-            answer = input1 + input2;
-            dataLibrary.push(`${answer} = ${input1} + ${input2}`);
+            //condition
+            answer = (Number(input1) + Number(input2));
+            data.push(`${input1} + ${input2} = ${answer}`);
             answers.push(answer);
             break;
 
         case '-':
-            answer = input1 - input2;
-            dataLibrary.push(`${answer} = ${input1} - ${input2}`);
+            //condition
+            answer = (Number(input1) - Number(input2));
+            data.push(`${input1} - ${input2} = ${answer}`);
             answers.push(answer);
             break;
-        case '*':
-            answer = input1 * input2;
-            dataLibrary.push(`${answer} = ${input1} * ${input2}`);
+        case 'x':
+            //condition
+            answer = (Number(input1) * Number(input2));
+            data.push(`${input1} * ${input2} = ${answer}`);
             answers.push(answer);
             break;
-        case '/':
-            answer = input1 / input2;
-            dataLibrary.push(`${answer} = ${input1} / ${input2}`);
+        case '÷':
+            answer = (Number(input1) / Number(input2));
+            data.push(`${input1} / ${input2} = ${answer}`);
             answers.push(answer);
             break;
     }
-}
+}//end of calculate Function.
+
+
+
